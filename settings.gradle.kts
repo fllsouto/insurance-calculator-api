7bde76c82d4d2bcedc774b84d00000127e40391b
Fellipe Souto <fllsouto@gmail.com>
2024-01-22 14:48:35 -0300

    infra: initial project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "calculo-seguro"
---
+++ src/test/java/br/com/itau/fllsouto/calculoseguro/CalculoSeguroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.fllsouto.calculoseguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoSeguroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
